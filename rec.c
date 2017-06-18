1a7c15297bf022baa5ff1cca576c736235b7b8cc
Peter Hofmann <scm@uninformativ.de>
2017-06-18 09:47:20 +0200

    rec: Record based on time and no auto-normalization
    Amplitude detection didn't work too well for me. :/
    
    rec now keeps 1/32s of history. As soon as we detect a key, that history
    will be written to the output file. After that, another 1/2s will be
    recorded to catch all the "zoingggggg" noises.
    
    Auto-normalization was a bad idea to begin with. We must find the global
    peak value of all samples and then normalize all of them using the same
    gain.

--- rec.c
+++ rec.c
@@ -13,7 +13,14 @@ struct input_event {
 	unsigned int value;
 };
 
-#define HISTSIZE 60
+/* We keep recording all the time, creating a history of BUF_HIST bytes.
+ * Once a key action has been registered, we store our history and
+ * record another BUF_POST bytes.
+ *
+ * Those exact values depend on your machine. We don't do
+ * auto-detection. */
+#define BUF_HIST (44100 / 32)
+#define BUF_POST (44100 / 2)
 
 void
 samplename(char *buf, size_t buf_len, unsigned short code, unsigned int value)
@@ -38,11 +45,10 @@ int main(int argc, char **argv)
 	char fname[256];
 	FILE *f;
 	int fd_snd;
-	int samples = 0;
 	int triggered = 0;
 	FILE *fout = NULL;
-	int head = 0;
-	int16_t hist[HISTSIZE];
+	int16_t hist[BUF_HIST], post[BUF_POST];
+	size_t hist_at = 0;
 
 	/* Run like:
 	 *
@@ -53,7 +59,7 @@ int main(int argc, char **argv)
 	fprintf(stderr, "Waiting for evdev data on stdin ...\n");
 
 	//f = popen("arecord -D plughw:1,0 -f cd -r 44100 -c 1", "r");
-	f = popen("parec --rate=44100 --format=s16le --channels=1", "r");
+	f = popen("parec --rate=44100 --format=s16le --channels=1 --latency=2", "r");
 	fd_snd = fileno(f);
 
 	fd_set fds;
@@ -73,47 +79,50 @@ int main(int argc, char **argv)
 
 			if(triggered == 0) {
 				samplename(fname, sizeof fname, event.code, event.value);
-				snprintf(cmd, sizeof cmd, "sox -qq -r 44100 -e signed -b 16 -c 1 -t raw --norm=-3 - %s", fname);
+				snprintf(cmd, sizeof cmd, "sox -qq -r 44100 -e signed -b 16 -c 1 -t raw - %s", fname);
 				printf("%02x %d: ", event.code, event.value);
 				fflush(stdout);
 				fout = popen(cmd, "w");
-				samples = 0;
 				triggered = 1;
 				printf("%02x %d ", event.code, event.value);
 				fflush(stdout);
 			}
 		}
 
-		if(FD_ISSET(fd_snd, &fds)) {
-			int16_t buf;
-			read(fd_snd, &buf, sizeof buf);
+		if (triggered)
+		{
+			size_t history_i = 0;
+			printf(">");
+			fflush(stdout);
 
-			if(triggered) {
-				if(samples == 0) {
-					if(buf < -1000 || buf > 1000) {
-						samples = 1;
-						printf(">");
-						fflush(stdout);
-					}
-				}
-
-				if(fout && samples) {
-					fwrite(&hist[head], 1, sizeof hist[head], fout);
-					if(samples > 44000/3) {
-						fclose(fout);
-						snprintf(cmd, sizeof(cmd), "paplay %s &", fname);
-						system(cmd);
-						printf(".\n");
-						fout = NULL;
-						triggered = 0;
-					}
-				}
-
-				if(samples) samples ++;
+			/* Write history. */
+			for (history_i = 0; history_i < BUF_HIST; history_i++)
+			{
+				hist_at++;
+				hist_at %= BUF_HIST;
+				fwrite(&hist[hist_at], sizeof (int16_t), 1, fout);
 			}
 
-			hist[head] = buf;
-			head = (head + 1) % HISTSIZE;
+			/* Record additional samples. */
+			fread(&post, BUF_POST, 1, f);
+			fwrite(&post, BUF_POST, 1, fout);
+
+			/* Close pipe and play back for examination. */
+			fclose(fout);
+			snprintf(cmd, sizeof(cmd), "paplay %s &", fname);
+			system(cmd);
+			printf(".\n");
+			fout = NULL;
+			triggered = 0;
+			hist_at = 0;
+		}
+
+		if(FD_ISSET(fd_snd, &fds)) {
+			/* Audio data ready to read but we haven't registered a key,
+			 * so add those two bytes to our history. */
+			read(fd_snd, &hist[hist_at], sizeof (int16_t));
+			hist_at++;
+			hist_at %= BUF_HIST;
 		}
 
 	}
